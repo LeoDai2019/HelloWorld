16a6505f482768255cffa1829065a382bb627350
LeoDai2019 <47370966+LeoDai2019@users.noreply.github.com>
2019-02-06 17:51:35 -0800

    Update Hello.java
    added by remote master

--- HelloWorld/src/Hello.java
+++ HelloWorld/src/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("hello world");
 		System.out.println("hello world added by branch 1");
+		System.out.println("hello world added by remote master");
 
 	}
 
